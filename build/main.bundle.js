8f53352d05201148e7ef5f4696f00d9fb498cb07
Kevin Etore <kevin.etore@nedap.com>
2017-09-04 21:40:01 +0200

    Made part of day dynamic and imported Jquery to be more efficient regarding the writing js to html

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,5 +1,7 @@
 "use strict";
 
 function current_time() {
-  var e = new Date();document.getElementById("current_time").innerHTML = e.toLocaleTimeString();
-}setInterval(current_time, 1e3);
+  var t = new Date();$("#current_time").html(t.toLocaleTimeString());
+}setInterval(current_time, 1e3), window.onload = function () {
+  var t = new Date().getHours();t >= 6 && t < 12 && $("#part_of_day").html("morning"), t >= 12 && t < 18 && $("#part_of_day").html("afternoon"), (t >= 18 || t < 6) && $("#part_of_day").html("evening");
+};
--- js/current_time.js
+++ js/current_time.js
@@ -2,5 +2,19 @@ setInterval(current_time ,1000);
 
 function current_time() {
     var d = new Date();
-    document.getElementById("current_time").innerHTML = d.toLocaleTimeString();
+    $('#current_time').html(d.toLocaleTimeString());
+}
+
+window.onload = function part_of_day() {
+  var time = new Date();
+  var part = time.getHours();
+  if (part >= 6 && part < 12) {
+    $("#part_of_day").html("morning")
+  }
+  if (part >= 12 && part < 18) {
+    $("#part_of_day").html("afternoon")
+  }
+  if (part >= 18 || part < 6){
+    $("#part_of_day").html("evening")
+  }
 }
